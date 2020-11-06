34e9df27c5ac6cddc6b24147e68ae9090871eb97
Tebogo <tn.selahle@gmail.com>
2020-11-06 23:35:43 +0200

    Submit

--- src/app/services/content.service.ts
+++ src/app/services/content.service.ts
@@ -14,35 +14,35 @@ export class ContentService {
       title: 'Website',
       description: 'An elegant website made with a sleek and modern approach',
       url: 'assets/img/web.png',
-      vid: 'assets/vid/web.mp4'
+      vid: 'assets/vids/web.mp4'
     },
     {
       id: 1,
       title: 'Imitation (Short Film)',
       description: 'A short horror film about a family that has bad things happen to them. The family is actually part of an AI simulation to test human emotional intelligence. Dead humans from the simulation are replaced so the simulation can continue',
       url: 'assets/img/YMI_short-film.png',
-      vid: 'assets/vid/YMI_short-film.mp4'
+      vid: 'assets/vids/YMI_short-film.mp4'
     },
     {
       id: 2,
       title: 'Ascent (Game)',
       description: 'This is an RPG game created by the students FOR the students. Jump in to enjoy yourself in this fun-packed game.',
       url: 'assets/img/Ascend.png',
-      vid: 'assets/vid/Ascend.mp4'
+      vid: 'assets/vids/Ascend.mp4'
     },
     {
       id: 3,
       title: 'Blogging website',
       description: 'A blogging website created for multiple purposes. Well-suited for personally blogs. It is very versatile and well designed.',
       url: 'assets/img/blog.png',
-      vid: 'assets/vid/blog.mp4'
+      vid: 'assets/vids/blog.mp4'
     },
     {
       id: 4,
       title: 'Idea (Short Film)',
       description: 'An idea-driven movie all about ideas. This thriller movie incorporates thriller and horror movies.',
       url: 'assets/img/short-film.png',
-      vid: 'assets/vid/short-film.mp4'
+      vid: 'assets/vids/short-film.mp4'
     },
     ];
 
--- src/app/view/view.component.ts
+++ src/app/view/view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
@@ -8,13 +8,28 @@ import {ActivatedRoute, Router} from '@angular/router';
 })
 export class ViewComponent implements OnInit {
 
-  content: any;
-
   constructor(private router: Router, private activatedRoute: ActivatedRoute) {
     this.content = this.router.getCurrentNavigation().extras.state;
   }
 
+  content: any;
+  public show = false;
+  public opened = false;
+
   ngOnInit(): void {
   }
 
+  // tslint:disable-next-line:typedef
+  toggleVideo() {
+    this.opened = true;
+    this.show = !this.show;
+  }
+
+  // tslint:disable-next-line:typedef
+  closeVideo() {
+    if (this.opened) {
+      this.show = false;
+      this.opened = false;
+    }
+  }
 }
